5c08a023ea4e08cfa107463642b65a404a37c071
MarcosViniSil <marcossilv203@gmail.com>
2025-04-25 18:59:08 -0300

    feat: add list of stop words

--- src/lexicalAnalyzer/LexicalAnalyzer.java
+++ src/lexicalAnalyzer/LexicalAnalyzer.java
@@ -21,133 +21,24 @@ public class LexicalAnalyzer {
 
     private HashMap<Character, Boolean> symbolsAvailable(){
         HashMap<Character, Boolean> symbols = new HashMap<>();
-        symbols.put('!', true);
-        symbols.put('"', true);
-        symbols.put('#', true);
-        symbols.put('$', true);
-        symbols.put('%', true);
-        symbols.put('&', true);
-        symbols.put('\'', true);
-        symbols.put('(', true);
-        symbols.put(')', true);
-        symbols.put('*', true);
-        symbols.put('+', true);
-        symbols.put(',', true);
-        symbols.put('-', true);
-        symbols.put('.', true);
-        symbols.put('/', true);
-        symbols.put('0', true);
-        symbols.put('1', true);
-        symbols.put('2', true);
-        symbols.put('3', true);
-        symbols.put('4', true);
-        symbols.put('5', true);
-        symbols.put('6', true);
-        symbols.put('7', true);
-        symbols.put('8', true);
-        symbols.put('9', true);
-        symbols.put(':', true);
-        symbols.put(';', true);
-        symbols.put('<', true);
-        symbols.put('=', true);
-        symbols.put('>', true);
-        symbols.put('?', true);
-        symbols.put('@', true);
-        symbols.put('A', true);
-        symbols.put('B', true);
-        symbols.put('C', true);
-        symbols.put('D', true);
-        symbols.put('E', true);
-        symbols.put('F', true);
-        symbols.put('G', true);
-        symbols.put('H', true);
-        symbols.put('I', true);
-        symbols.put('J', true);
-        symbols.put('K', true);
-        symbols.put('L', true);
-        symbols.put('M', true);
-        symbols.put('N', true);
-        symbols.put('O', true);
-        symbols.put('P', true);
-        symbols.put('Q', true);
-        symbols.put('R', true);
-        symbols.put('S', true);
-        symbols.put('T', true);
-        symbols.put('U', true);
-        symbols.put('V', true);
-        symbols.put('W', true);
-        symbols.put('X', true);
-        symbols.put('Y', true);
-        symbols.put('Z', true);
-        symbols.put('[', true);
-        symbols.put('\\', true);
-        symbols.put(']', true);
-        symbols.put('^', true);
-        symbols.put('_', true);
-        symbols.put('`', true);
-        symbols.put('´', true);
-        symbols.put('a', true);
-        symbols.put('b', true);
-        symbols.put('c', true);
-        symbols.put('d', true);
-        symbols.put('e', true);
-        symbols.put('f', true);
-        symbols.put('g', true);
-        symbols.put('h', true);
-        symbols.put('i', true);
-        symbols.put('j', true);
-        symbols.put('k', true);
-        symbols.put('l', true);
-        symbols.put('m', true);
-        symbols.put('n', true);
-        symbols.put('o', true);
-        symbols.put('p', true);
-        symbols.put('q', true);
-        symbols.put('r', true);
-        symbols.put('s', true);
-        symbols.put('t', true);
-        symbols.put('u', true);
-        symbols.put('v', true);
-        symbols.put('w', true);
-        symbols.put('x', true);
-        symbols.put('y', true);
-        symbols.put('z', true);
-        symbols.put('{', true);
-        symbols.put('|', true);
-        symbols.put('}', true);
-        symbols.put('~', true);
-        symbols.put('À', true);
-        symbols.put('Á', true);
-        symbols.put('Â', true);
-        symbols.put('Ã', true);
-        symbols.put('Ç', true);
-        symbols.put('É', true);
-        symbols.put('Ê', true);
-        symbols.put('Í', true);
-        symbols.put('Ó', true);
-        symbols.put('Ô', true);
-        symbols.put('Õ', true);
-        symbols.put('Ú', true);
-        symbols.put('à', true);
-        symbols.put('á', true);
-        symbols.put('â', true);
-        symbols.put('ã', true);
-        symbols.put('ç', true);
-        symbols.put('é', true);
-        symbols.put('ê', true);
-        symbols.put('í', true);
-        symbols.put('ó', true);
-        symbols.put('ô', true);
-        symbols.put('õ', true);
-        symbols.put('ú', true);
-        symbols.put('ª', true);
-        symbols.put('º', true);
-        symbols.put('°', true);
-        symbols.put('¨', true);
-        symbols.put('§', true);
+        char[] validSymbols = {'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', '´', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~','À', 'Á', 'Â', 'Ã', 'Ç', 'É', 'Ê', 'Í', 'Ó', 'Ô', 'Õ', 'Ú', 'à', 'á', 'â', 'ã', 'ç', 'é', 'ê', 'í', 'ó', 'ô', 'õ', 'ú', 'ª', 'º', '°', '¨', '§'};
 
+        for (char symbol : validSymbols) {
+            symbols.put(symbol, true);
+        }
 
         return symbols;
 
+    }
+
+    public HashMap<Character, Boolean> stopWordsList(){
+        HashMap<String, Boolean> stopWords = new HashMap<>();
+        String[] words = {"a", "à", "ao", "aos", "aquela", "aquelas", "aquele", "aqueles", "aquilo", "as", "às", "até", "com", "como", "da", "das", "de", "dela", "delas", "dele", "deles", "depois", "do", "dos", "e", "é", "ela", "elas", "ele", "eles", "em", "entre", "era", "eram", "éramos", "essa", "essas", "esse", "esses", "esta", "está", "estamos", "estão", "estar", "estas", "estava", "estavam", "estávamos", "este", "esteja", "estejam", "estejamos", "estes", "esteve", "estive", "estivemos", "estiver", "estivera", "estiveram", "estivéramos", "estiverem", "estivermos", "estivesse", "estivessem", "estivéssemos", "estou", "eu", "foi", "fomos", "for", "fora", "foram", "fôramos", "forem", "formos", "fosse", "fossem", "fôssemos", "fui", "há", "haja", "hajam", "hajamos", "hão", "havemos", "haver", "hei", "houve", "houvemos", "houver", "houvera", "houverá", "houveram", "houvéramos", "houverão", "houverei", "houverem", "houveremos", "houveria", "houveriam", "houveríamos", "houvermos", "houvesse", "houvessem", "houvéssemos", "isso", "isto", "já", "lhe", "lhes", "mais", "mas", "me", "mesmo", "meu", "meus", "minha", "minhas", "muito", "na", "não", "nas", "nem", "no", "nos", "nós", "nossa", "nossas", "nosso", "nossos", "num", "numa", "o", "os", "ou", "para", "pela", "pelas", "pelo", "pelos", "por", "qual", "quando", "que", "quem", "são", "se", "seja", "sejam", "sejamos", "sem", "ser", "será", "serão", "serei", "seremos", "seria", "seriam", "seríamos", "seu", "seus", "só", "somos", "sou", "sua", "suas", "também", "te", "tem", "tém", "temos", "tenha", "tenham", "tenhamos", "tenho", "terá", "terão", "terei", "teremos", "teria", "teriam", "teríamos", "teu", "teus", "teve", "tinha", "tinham", "tínhamos", "tive", "tivemos", "tiver", "tivera", "tiveram", "tivéramos", "tiverem", "tivermos", "tivesse", "tivessem", "tivéssemos", "tu", "tua", "tuas", "um", "uma", "você", "vocês", "vos"};
+       
+        for (String word : words) {
+            stopWords.put(word, true);
+        }
+
+        return stopWords;
     }
 }
