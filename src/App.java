8600551c7a29bce090648f7668814bd4fee06f8f
MarcosViniSil <marcossilv203@gmail.com>
2025-04-25 20:20:22 -0300

    feat: create function to verify if two words are similar

--- src/App.java
+++ src/App.java
@@ -3,8 +3,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         LexicalAnalyzer la = new LexicalAnalyzer();
         try {
-            la.lexicalVerification("teste de sucesso!!! ");
-            la.lexicalVerification("teste de falha ж");
+            System.out.println(la.similarityWithDifferenceTwoCharacters("excecao","excessao"));
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
--- src/lexicalAnalyzer/LexicalAnalyzer.java
+++ src/lexicalAnalyzer/LexicalAnalyzer.java
@@ -19,6 +19,54 @@ public class LexicalAnalyzer {
 
     }
 
+    public boolean similarityWithDifferenceTwoCharacters(String word1, String word2){
+
+        word1 = this.removeAccentIfExists(word1.toLowerCase());
+        word2 = this.removeAccentIfExists(word2.toLowerCase());
+    
+        int minLen = Math.min(word1.length(), word2.length());
+        int diff = Math.abs(word1.length() - word2.length()); 
+    
+        for (int i = 0; i < minLen; i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                diff++;
+            }
+        }
+    
+        return diff <= 2;
+    }
+
+    private String removeAccentIfExists(String word){
+        
+        for (int i = 0; i < word.length(); i++) {
+            char letterModify = this.characterAnAccent(word.charAt(i));
+            if(letterModify != ' '){
+                word = word.replace(word.charAt(i), letterModify);
+            }
+        }
+
+        return word;
+    }
+
+    private char characterAnAccent(char letter){
+       
+        if (letter == 'à' || letter == 'á' || letter == 'â' || letter == 'ã' ){
+            return 'a';
+        }else if(letter == 'ç'){
+            return 'c';
+        }else if(letter == 'é' || letter == 'ê'){
+            return 'e';
+        }else if(letter == 'í'){
+            return 'i';
+        }else if(letter == 'ó' || letter == 'ô' || letter == 'õ'){
+            return 'o';
+        }else if(letter == 'ú'){
+            return 'u';
+        }
+        
+        return ' ';
+    }
+
     private HashMap<Character, Boolean> symbolsAvailable(){
         HashMap<Character, Boolean> symbols = new HashMap<>();
         char[] validSymbols = {'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/','0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', '´', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~','À', 'Á', 'Â', 'Ã', 'Ç', 'É', 'Ê', 'Í', 'Ó', 'Ô', 'Õ', 'Ú', 'à', 'á', 'â', 'ã', 'ç', 'é', 'ê', 'í', 'ó', 'ô', 'õ', 'ú', 'ª', 'º', '°', '¨', '§'};
